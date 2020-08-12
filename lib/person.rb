2fc242b7d863502276e3a8094210c1f48c437749
Anthony Lepore <violacellonyc@gmail.com>
2020-08-12 18:14:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,8 @@ class Person
   attr_accessor :weight, :handed, :complexion, :t_shirt_size 
   
   def initalize(attributes)
-  
-  
+   attributes.each {|key, value| self.send(("#{key}="), value)}
 end
+
+
 end
