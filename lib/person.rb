1e9de6713a48e105aa77584b0c1b1992f74266f0
Anthony Lepore <violacellonyc@gmail.com>
2020-08-12 18:18:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,9 @@
 class Person
   #your code here
-  attr_accessor :weight, :handed, :complexion, :t_shirt_size 
+  attr_accessor :weight, :handed, :complexion, :t_shirt_size, 
+  
+  
+  
   
   def initalize(attributes)
    attributes.each {|key, value| self.send(("#{key}="), value)}
